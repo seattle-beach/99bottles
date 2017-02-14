f0f31448c4c7907863801e62617dc6fb65bf336d
Gregg Van Hove <gvanhove@pivotal.io>
2017-02-14 10:28:35 -0800

    Use some ruby stuff and re-duplicate 'of beer'

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,12 +4,12 @@ class Bottles
   end
 
   def verses(starting_number, final_number)
-    (final_number..starting_number).to_a.reverse.map { |number| verse(number) }.join("\n")
+    starting_number.downto(final_number).map { |number| verse(number) }.join("\n")
   end
 
   def verse(number)
-    line_one = "#{start_sentence(bottle_count(number))} on the wall, #{bottle_count(number)}."
-    line_two = "Take #{number == 1 ? 'it' : 'one'} down and pass it around, #{bottle_count(number - 1)} on the wall."
+    line_one = "#{bottle_count(number).capitalize} of beer on the wall, #{bottle_count(number)} of beer."
+    line_two = "Take #{number == 1 ? 'it' : 'one'} down and pass it around, #{bottle_count(number - 1)} of beer on the wall."
 
     if number == 0
       line_two = "Go to the store and buy some more, 99 bottles of beer on the wall."
@@ -22,13 +22,9 @@ class Bottles
 
   def bottle_count(number)
     if number > 0
-      "#{number} bottle#{number == 1 ? '' : 's'} of beer"
+      "#{number} bottle#{number == 1 ? '' : 's'}"
     else
-      "no more bottles of beer"
+      "no more bottles"
     end
-  end
-
-  def start_sentence(words)
-    "#{words[0].upcase}#{words[1..-1]}"
   end
 end
